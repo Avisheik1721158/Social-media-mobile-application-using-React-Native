db5a3d36a1a3394c7fa5ba62bb41a2dc83df3170
Avisheik Barua <avisheikbarua960@gmail.com>
2023-12-16 21:14:09 +0600

    Initial UI files

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+// import { View } from "react-native";
+// export default function App() {
+//   return (
+//     <View style={{ flex: 1, backgroundColor: "plum" }}>
+//       <View
+//         style={{ width: 100, height: 100, backgroundColor: "lightblue" }}
+//       ></View>
+//       <View
+//         style={{ width: 100, height: 100, backgroundColor: "lightgreen" }}
+//       ></View>
+//     </View>
+//   );
+// }
+
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import OnBoardingScreen from "./screens/OnBoardingScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+const AppStack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Onboarding" component={OnBoardingScreen} />
+        <AppStack.Screen name="Login" component={LoginScreen} />
+      </AppStack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App;
